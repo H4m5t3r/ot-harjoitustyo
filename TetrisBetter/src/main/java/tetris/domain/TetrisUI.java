fb8b7ec7646c9e2a1ead86f3bb0eb3450fd81948
TheHamster <anton.taleiko@gmail.com>
2020-04-19 18:48:50 +0300

    Added some menu elements

--- TetrisBetter/src/main/java/tetris/domain/TetrisUI.java
+++ TetrisBetter/src/main/java/tetris/domain/TetrisUI.java
@@ -8,7 +8,12 @@ package tetris.domain;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -16,18 +21,71 @@ import javafx.stage.Stage;
  * @author taleiko
  */
 public class TetrisUI extends Application {
+    private Scene menuScene;
+    private BorderPane menuPane;
+    private GridPane menuElements;
+    private Label title;
+    private Label music;
+    private Button newGame;
+    private Button eightBit;
+    private Button piano;
+    private Button trumpet;
+    private VBox musicButtons;
+    
+    private Scene gameScene;
+    private Pane gamePane;
+    
 
     @Override
+    public void init() {
+        menuPane = new BorderPane();
+        newGame = new Button("Start a new game");
+        menuElements = new GridPane();
+        title = new Label("TETRIS");
+        music = new Label("Music");
+        eightBit = new Button("8-bit");
+        piano = new Button("Piano");
+        trumpet = new Button("Trumpet");
+        musicButtons = new VBox();
+    }
+    
+    @Override
     public void start(Stage ikkuna) throws Exception {
-        Button nappi = new Button("Tämä on nappi");
-
-        FlowPane komponenttiryhma = new FlowPane();
-        komponenttiryhma.getChildren().add(nappi);
-
-        Scene nakyma = new Scene(komponenttiryhma);
-
-        ikkuna.setScene(nakyma);
+        //The menu
+        menuElements.add(newGame, 2, 3);
+        menuElements.add(title, 2, 1);
+        musicButtons.getChildren().add(music);
+        musicButtons.getChildren().add(eightBit);
+        musicButtons.getChildren().add(piano);
+        musicButtons.getChildren().add(trumpet);
+        
+        menuElements.add(musicButtons, 2, 2);
+        
+        menuPane.setCenter(menuElements);
+        
+        
+        
+        
+        //The game screen
+        gamePane = new Pane();
+        gameScene = new Scene(gamePane, 500, 500);
+        
+        
+        
+        menuScene = new Scene(menuPane, 500, 500);
+        
+        newGame.setOnAction((event) -> {
+          ikkuna.setScene(gameScene);
+      });
+        
+        ikkuna.setScene(menuScene);
         ikkuna.show();
+    }
+    
+    @Override
+    public void stop() {
+      // tee lopetustoimenpiteet täällä
+      System.out.println("Game closing");
     }
     
     public static void main(String[] args) {
