93e3fa51140f08adbe0a65be63149c2831036de9
TheHamster <anton.taleiko@gmail.com>
2020-04-21 15:45:15 +0300

    Sequence diagram for createRandomTetramino() added

--- TetrisBetter/src/main/java/tetris/ui/TetrisUI.java
+++ TetrisBetter/src/main/java/tetris/ui/TetrisUI.java
@@ -9,7 +9,6 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import static javafx.scene.input.KeyCode.DOWN;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
